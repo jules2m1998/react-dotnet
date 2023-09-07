da9659a34716c72b72b15ad9c1932f022407375e
jules2m1998 <mevaajules9@gmail.com>
2023-09-07 14:24:55 +0400

    ✨feature: front end architecture definition

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import TodoListView from "./Presentation/Todo/TodoList/TodoListView";
+
+function App() {
+  return (
+    <>
+      <p>Test</p>
+      <TodoListView />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/Domain/UseCases/Readme.md
@@ -0,0 +1,3 @@
+# Use cases
+
+**UseCases:** Container to list all functionality of our application. e.g `Get Todos`, `Delete Todo`, `Create Todo`, `Update Todo`.
